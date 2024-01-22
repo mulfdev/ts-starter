8bf9dd2d52d3304b4064280de3d3f09a41cc17d3
mulf <mhmulford0@gmail.com>
2024-01-22 10:45:28 -0600

    split files and update bg text

--- src/scripts/exportData.ts
+++ src/scripts/exportData.ts
@@ -1,14 +1,39 @@
+// @ts-nocheck
 import { writeFileSync } from "fs";
 import { db } from "../utils.js";
-import { proposals } from "../db/schema.js";
+import { proposals, votes } from "../db/schema.js";
+import { eq } from "drizzle-orm";
 
-const props = await db.select().from(proposals).limit(5);
+const props = await db.select().from(proposals);
 
 // writeFileSync()
+let numFiles = 1;
 
-const propObj = [];
-for (const prop of props) {
-  propObj.push(prop);
+async function writeFile() {
+  let propObj = [];
+
+  for (const prop of props) {
+    if (prop.id % 50 === 0) {
+      writeFileSync(
+        `./src/export/data${numFiles}.json`,
+        JSON.stringify(propObj)
+      );
+      numFiles++;
+
+      propObj = [];
+    }
+    const votesInfo = await db
+      .select({
+        support: votes.support,
+        voter: votes.voter,
+        numberOfVotes: votes.votes,
+        reason: votes.reason,
+      })
+      .from(votes)
+      .where(eq(votes.proposalId, prop.id));
+    propObj.push({ prop, votes: [] });
+    propObj.find((item) => item.prop.id === prop.id)?.votes.push(...votesInfo);
+  }
 }
 
-writeFileSync("./src/export/data.json", JSON.stringify(propObj));
+await writeFile();
